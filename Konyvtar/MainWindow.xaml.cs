266b2870559dbe84b870fa2f4a5396abc63f4991
princzpatrik <princzpatrik3@gmail.com>
2025-11-25 13:30:58 +0100

    ertesitesek es tagsag

--- Konyvtar/MainWindow.xaml.cs
+++ Konyvtar/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,14 +12,37 @@ using System.Windows.Shapes;
 
 namespace Konyvtar
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class MainWindow : Window
+	{
+		string fajl = "olvasok.txt";
+		List<Olvaso> olvasok = new List<Olvaso>();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+		
+		}
+
+		private void btnMentes_Click(object sender, RoutedEventArgs e)
+		{
+			if (!int.TryParse(tbEletkor.Text, out int eletkor))
+			{
+				tbUzenet.Text = "Hibás életkor!";
+				return;
+			}
+
+			ComboBoxItem kivalasztott = cbMufaj.SelectedItem as ComboBoxItem;
+			string mufaj = kivalasztott?.Content.ToString() ?? "";
+
+			List<string> ertesitesekList = new List<string>();
+			if (chkHirlevel.IsChecked == true) ertesitesekList.Add("Hírlevél");
+			if (chkSms.IsChecked == true) ertesitesekList.Add("SMS");
+			string ertesitesek = string.Join(", ", ertesitesekList);
+
+			string tagsag = "";
+			if (rbNormal.IsChecked == true) tagsag = "Normál";
+			else if (rbDiak.IsChecked == true) tagsag = "Diák";
+			else if (rbNyugdijas.IsChecked == true) tagsag = "Nyugdíjas";
+		}
+	}
 }
