79c553f0d6992265d0175a4cd374523ffaa06e2d
princzpatrik <princzpatrik3@gmail.com>
2025-11-25 13:17:46 +0100

    Add project files.

---
+++ Konyvtar/Olvaso.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konyvtar
+{
+	public class Olvaso
+	{
+		public string Nev { get; set; }
+		public int Eletkor { get; set; }
+		public string Mufaj { get; set; }
+		public string Ertesitesek { get; set; }
+		public string Tagsag { get; set; }
+
+		public override string ToString()
+		{
+			return Nev;  
+		}
+	}
+
+
+}
